9e23dcc830aee9d1c7e8a98d2d45259e25af0445
Samz31012006 <samarthhk1234@gmail.com>
2026-03-28 18:46:38 +0530

    security: move hardcoded firebase keys to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcyxFqi-MOnO2JGpA8aLW7tQ1NxQ3P9OY",
-  authDomain: "agriforecast01.firebaseapp.com",
-  projectId: "agriforecast01",
-  storageBucket: "agriforecast01.firebasestorage.app",
-  messagingSenderId: "608879399496",
-  appId: "1:608879399496:web:cb71bc534d8456e446fe7d",
-  measurementId: "G-H5YY5BH667"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
